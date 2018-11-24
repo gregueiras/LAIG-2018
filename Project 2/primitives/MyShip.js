b54ce8b7e7494a09a08ec22e0de09b8de7c09915
gregueiras <gregueiras@gmail.com>
2018-11-24 11:09:48 +0000

    Fixed the ship wings

--- Project 2/primitives/MyShip.js
+++ Project 2/primitives/MyShip.js
@@ -2,27 +2,30 @@ class MyShip {
   constructor(scene) {
     this.scene = scene;
 
-    const nParts = 6;
+    const nParts = 4;
     const zOffset = 2.5;
-    const yOffset = 1;
+    const yOffset = 2;
+    const yBase = -2;
+    const otherWeight = 0.707;
+    const xOffset = 1;
 
     let controlVertices = [
       [0, -yOffset, zOffset, 1],
       [0, -yOffset, 0, 1],
-      [1.8, -yOffset, zOffset, 0.707],
-      [1.8, -yOffset, 0, 0.707],
-      [1.8, 0, zOffset, 1],
-      [1.8, 0, 0, 1],
-      [1.8, yOffset, zOffset, 0.707],
-      [1.8, yOffset, 0, 0.707],
-      [0, yOffset, zOffset, 1],
-      [0, yOffset, 0, 1],
-      [1.9, yOffset, zOffset, 0.707],
-      [1.9, yOffset, 0, 0.707],
-      [1.9, 0, zOffset, 1],
-      [1.9, 0, 0, 1],
-      [1.9, -yOffset, zOffset, 0.707],
-      [1.9, -yOffset, 0, 0.707],
+      [xOffset - 0.01, -yOffset, zOffset, otherWeight],
+      [xOffset - 0.01, -yOffset, 0, otherWeight],
+      [xOffset - 0.01, yBase, zOffset, 1],
+      [xOffset - 0.01, yBase, 0, 1],
+      [xOffset - 0.01, yOffset, zOffset, otherWeight],
+      [xOffset - 0.01, yOffset, 0, otherWeight],
+      [-1.5, yOffset, zOffset, 1],
+      [-1.5, yOffset, 0, 1],
+      [xOffset, yOffset, zOffset, otherWeight],
+      [xOffset, yOffset, 0, otherWeight],
+      [xOffset, yBase, zOffset, 1],
+      [xOffset, yBase, 0, 1],
+      [xOffset, -yOffset, zOffset, otherWeight],
+      [xOffset, -yOffset, 0, otherWeight],
       [0, -yOffset, zOffset, 1],
       [0, -yOffset, 0, 1],
 
@@ -36,28 +39,46 @@ class MyShip {
     this.text_wing = new CGFtexture(this.scene, './scenes/images/dots.png');
     this.text_arms = new CGFtexture(this.scene, './scenes/images/Spaceship.jpg');
     this.text_ball = new CGFtexture(this.scene, './scenes/images/tie.jpg');
+
+    this.myShader = new CGFshader(this.scene.gl, '../lib/CGF/shaders/Gouraud/textured/multiple_light-vertex.glsl', './shaders/texture2.frag');
+
+    const options = {
+      timeFactor: 1,
+      normScale: 1,
+      myHeightmap: 1,
+      myTexture: 0,
+      textureScale: 2
+    }
+    this.myShader.setUniformsValues(options);
   }
 
   display() {
+    const xOffset = 2.3;
+    const yOffset = 0.58;
+    const zOffset = -1.2;
+
     this.scene.pushMatrix();
 
     this.scene.pushMatrix();
     this.text_wing.bind(0);
+    this.scene.setActiveShader(this.myShader);
 
-
+    this.scene.rotate(-5 * DEGREE_TO_RAD, 0, 0, 1);
     this.scene.rotate(180 * DEGREE_TO_RAD, 1, 0, 0);
-    this.scene.translate(1.55, 0.1, -1.2);
+    this.scene.translate(xOffset, yOffset, zOffset);
     this.side.display();
     this.scene.popMatrix();
-
+    
     this.scene.pushMatrix();
-    this.scene.translate(-1.55, -0.1, -1.2);
+    this.scene.rotate(5 * DEGREE_TO_RAD, 0, 0, 1);
+    this.scene.translate(-xOffset, -yOffset, zOffset);
     this.scene.rotate(180 * DEGREE_TO_RAD, 1, 0, 0);
     this.scene.rotate(180 * DEGREE_TO_RAD, 0, 1, 0);
     this.side.display();
 
     this.text_wing.unbind(0);
     this.scene.popMatrix();
+    this.scene.setActiveShader(this.scene.defaultShader);
 
 
     this.scene.pushMatrix();
